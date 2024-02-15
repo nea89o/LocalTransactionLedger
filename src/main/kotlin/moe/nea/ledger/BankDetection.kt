b828702de8724217ceabee178b33e30fffeaa07a
Linnea Gräf <nea@nea.moe>
2024-02-15 22:29:49 +0100

    Fix personal bank pattern

--- src/main/kotlin/moe/nea/ledger/BankDetection.kt
+++ src/main/kotlin/moe/nea/ledger/BankDetection.kt
@@ -5,9 +5,9 @@ import java.util.regex.Pattern
 
 class BankDetection(val ledger: LedgerLogger) {
     val withdrawPattern =
-        Pattern.compile("^You have withdrawn (?<amount>$SHORT_NUMBER_PATTERN) coins?! You now have (?<newtotal>$SHORT_NUMBER_PATTERN) coins? in your account!$")
+        Pattern.compile("^(You have withdrawn|Withdrew) (?<amount>$SHORT_NUMBER_PATTERN) coins?! (?:There's now|You now have) (?<newtotal>$SHORT_NUMBER_PATTERN) coins? (?:lef in the account!|in your account!)$")
     val depositPattern =
-        Pattern.compile("^You have deposited (?<amount>$SHORT_NUMBER_PATTERN) coins?! You now have (?<newtotal>$SHORT_NUMBER_PATTERN) coins? in your account!$")
+        Pattern.compile("^(?:You have deposited|Deposited) (?<amount>$SHORT_NUMBER_PATTERN) coins?! (?:There's now|You now have) (?<newtotal>$SHORT_NUMBER_PATTERN) coins? (?:in your account!|in the account!)$")
     @SubscribeEvent
     fun onChat(event: ChatReceived) {
         withdrawPattern.useMatcher(event.message) {
