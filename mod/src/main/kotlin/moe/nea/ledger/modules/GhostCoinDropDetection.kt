a8dc0dc3be2679605880b1948c61b7dfe2f35f10
jani270 <69345714+jani270@users.noreply.github.com>
2025-01-24 23:48:21 +0100

    feat: Ghost Coin Drop Detection

---
+++ mod/src/main/kotlin/moe/nea/ledger/modules/GhostCoinDropDetection.kt
@@ -0,0 +1,37 @@
+package moe.nea.ledger.modules
+
+import moe.nea.ledger.ItemChange
+import moe.nea.ledger.LedgerEntry
+import moe.nea.ledger.LedgerLogger
+import moe.nea.ledger.SHORT_NUMBER_PATTERN
+import moe.nea.ledger.TransactionType
+import moe.nea.ledger.events.ChatReceived
+import moe.nea.ledger.parseShortNumber
+import moe.nea.ledger.useMatcher
+import moe.nea.ledger.utils.di.Inject
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.util.regex.Pattern
+
+class GhostCoinDropDetection {
+
+    val ghostCoinPattern =
+        Pattern.compile("The ghost's death materialized (?<coins>$SHORT_NUMBER_PATTERN) coins from the mists!")
+
+    @Inject
+    lateinit var logger: LedgerLogger
+
+    @SubscribeEvent
+    fun onGhostCoinDrop(event: ChatReceived) {
+        ghostCoinPattern.useMatcher(event.message) {
+            logger.logEntry(
+                LedgerEntry(
+                    TransactionType.GHOST_COIN_DROP,
+                    event.timestamp,
+                    listOf(
+                        ItemChange.gainCoins(parseShortNumber(group("coins"))),
+                    )
+                )
+            )
+        }
+    }
+}
